a41ae4abe3a7f23f55a405f67f3124228825c068
Garret Katayama <garret.katayama@gmail.com>
2022-06-13 01:29:27 -0600

    api connected

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
         <input 
@@ -13,10 +13,10 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">{{ }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }}</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }} C</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
@@ -48,7 +48,16 @@ export default {
       this.weather = results;
     },
     dateBuilder () {
-      //let d = new Date();
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day}, ${date} ${month} ${year}`;
     }
   }
 }
@@ -70,6 +79,10 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.5s;
+}
+
+#app.warm {
+  background-image: url('./assets/warm-bg.jpg');
 }
 
 main {
