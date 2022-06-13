f339e7e211566cd5ebfb2e794fcbc4b3b7cd3d0f
Garret Katayama <garret.katayama@gmail.com>
2022-06-13 09:50:42 -0600

    api connected

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@
           <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">{{ Math.round(weather.main.temp) }} C</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}° C</div>
           <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
