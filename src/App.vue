d1f3a85953cc03e28d1400c38b124b38ce39103a
Garret Katayama <garret.katayama@gmail.com>
2022-06-13 10:34:38 -0600

    api connected

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">{{ dateBuilder() }}</div>
+          <div class="date">{{ getFormattedDate() }}</div>
         </div>
         <div class="weather-box">
           <div class="temp">{{ Math.round(weather.main.temp) }}° C</div>
@@ -47,7 +47,7 @@ export default {
     setResults(results) {
       this.weather = results;
     },
-    dateBuilder () {
+    getFormattedDate () {
       let d = new Date();
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
